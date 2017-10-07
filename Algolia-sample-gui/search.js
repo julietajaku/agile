da97a023803f9967c130bb22a47f2fc32ec3636d
Julieta Jakubowicz <julietajaku@gmail.com>
2017-10-06 21:46:58 -0400

    Working GUI POC
    Based off of a template provided by Algol

--- Algolia-sample-gui/search.js
+++ Algolia-sample-gui/search.js
@@ -2,9 +2,9 @@
 /* global instantsearch */
 
 var search = instantsearch({
-  appId: 'latency',
-  apiKey: '0334f7b249a74454c67ee168e9b727b2',
-  indexName: 'Strengths Public Content Coaching'
+  appId: 'UL5C2EMZSX',
+  apiKey: '8c3fb88ff1733c780fa1bfbc34a96a3a',
+  indexName: 'Agile'
 });
 
 search.addWidget(
@@ -31,14 +31,14 @@ search.on('render', function() {
 
 var hitTemplate =
   '<article class="hit">' +
-      '<div class="product-picture-wrapper">' +
-        '<div class="product-picture"><img src="{{image}}" /></div>' +
-      '</div>' +
+      // '<div class="product-picture-wrapper">' +
+      //   '<div class="product-picture"><img src="{{image}}" /></div>' +
+      // '</div>' +
       '<div class="product-desc-wrapper">' +
-        '<div class="product-name">{{{_highlightResult.name.value}}}</div>' +
-        '<div class="product-type">{{{_highlightResult.type.value}}}</div>' +
-        '<div class="product-price">${{price}}</div>' +
-        '<div class="product-rating">{{#stars}}<span class="ais-star-rating--star{{^.}}__empty{{/.}}"></span>{{/stars}}</div>' +
+        '<div class="product-name">{{{_highlightResult.Question.value}}}</div>' +
+        '<div class="product-type">{{{_highlightResult.Answer.value}}}</div>' +
+        // '<div class="product-price">${{price}}</div>' +
+        // '<div class="product-rating">{{#stars}}<span class="ais-star-rating--star{{^.}}__empty{{/.}}"></span>{{/stars}}</div>' +
       '</div>' +
   '</article>';
 
@@ -89,91 +89,91 @@ search.addWidget(
   })
 );
 
-search.addWidget(
-  instantsearch.widgets.hierarchicalMenu({
-    container: '#categories',
-    attributes: ['category', 'sub_category', 'sub_sub_category'],
-    sortBy: ['name:asc'],
-    templates: {
-      item: menuTemplate
-    }
-  })
-);
+// search.addWidget(
+//   instantsearch.widgets.hierarchicalMenu({
+//     container: '#categories',
+//     attributes: ['category', 'sub_category', 'sub_sub_category'],
+//     sortBy: ['name:asc'],
+//     templates: {
+//       item: menuTemplate
+//     }
+//   })
+// );
 
-search.addWidget(
-  instantsearch.widgets.refinementList({
-    container: '#materials',
-    attributeName: 'materials',
-    operator: 'or',
-    limit: 10,
-    templates: {
-      item: facetTemplateCheckbox,
-      header: '<div class="facet-title">Materials</div class="facet-title">'
-    }
-  })
-);
+// search.addWidget(
+//   instantsearch.widgets.refinementList({
+//     container: '#materials',
+//     attributeName: 'materials',
+//     operator: 'or',
+//     limit: 10,
+//     templates: {
+//       item: facetTemplateCheckbox,
+//       header: '<div class="facet-title">Materials</div class="facet-title">'
+//     }
+//   })
+// );
 
-search.addWidget(
-  instantsearch.widgets.refinementList({
-    container: '#colors',
-    attributeName: 'colors',
-    operator: 'or',
-    limit: 10,
-    templates: {
-      item: facetTemplateColors,
-      header: '<div class="facet-title">Colors</div class="facet-title">'
-    }
-  })
-);
+// search.addWidget(
+//   instantsearch.widgets.refinementList({
+//     container: '#colors',
+//     attributeName: 'colors',
+//     operator: 'or',
+//     limit: 10,
+//     templates: {
+//       item: facetTemplateColors,
+//       header: '<div class="facet-title">Colors</div class="facet-title">'
+//     }
+//   })
+// );
 
-search.addWidget(
-  instantsearch.widgets.starRating({
-    container: '#rating',
-    attributeName: 'rating',
-    templates: {
-      header: '<div class="facet-title">Ratings</div class="facet-title">'
-    }
-  })
-);
+// search.addWidget(
+//   instantsearch.widgets.starRating({
+//     container: '#rating',
+//     attributeName: 'rating',
+//     templates: {
+//       header: '<div class="facet-title">Ratings</div class="facet-title">'
+//     }
+//   })
+// );
 
-search.addWidget(
-  instantsearch.widgets.priceRanges({
-    container: '#prices',
-    attributeName: 'price',
-    cssClasses: {
-      list: 'nav nav-list',
-      count: 'badge pull-right',
-      active: 'active'
-    },
-    templates: {
-      header: '<div class="facet-title">Prices</div class="facet-title">'
-    }
-  })
-);
+// search.addWidget(
+//   instantsearch.widgets.priceRanges({
+//     container: '#prices',
+//     attributeName: 'price',
+//     cssClasses: {
+//       list: 'nav nav-list',
+//       count: 'badge pull-right',
+//       active: 'active'
+//     },
+//     templates: {
+//       header: '<div class="facet-title">Prices</div class="facet-title">'
+//     }
+//   })
+// );
 
-search.addWidget(
-  instantsearch.widgets.sortBySelector({
-    container: '#sort-by-selector',
-    indices: [
-      {name: 'ikea', label: 'Featured'},
-      {name: 'ikea_price_asc', label: 'Price asc.'},
-      {name: 'ikea_price_desc', label: 'Price desc.'}
-    ],
-    label:'sort by'
-  })
-);
+// search.addWidget(
+//   instantsearch.widgets.sortBySelector({
+//     container: '#sort-by-selector',
+//     indices: [
+//       {name: 'ikea', label: 'Featured'},
+//       {name: 'ikea_price_asc', label: 'Price asc.'},
+//       {name: 'ikea_price_desc', label: 'Price desc.'}
+//     ],
+//     label:'sort by'
+//   })
+// );
 
-search.addWidget(
-  instantsearch.widgets.clearAll({
-    container: '#clear-all',
-    templates: {
-      link: '<i class="fa fa-eraser"></i> Clear all filters'
-    },
-    cssClasses: {
-      root: 'btn btn-block btn-default'
-    },
-    autoHideContainer: true
-  })
-);
+// search.addWidget(
+//   instantsearch.widgets.clearAll({
+//     container: '#clear-all',
+//     templates: {
+//       link: '<i class="fa fa-eraser"></i> Clear all filters'
+//     },
+//     cssClasses: {
+//       root: 'btn btn-block btn-default'
+//     },
+//     autoHideContainer: true
+//   })
+// );
 
 search.start();
